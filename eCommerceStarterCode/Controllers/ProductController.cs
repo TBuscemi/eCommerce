45ed36a938b42cb7e374d029b2c1a31bbd55e487
TBuscemi <thomasbuscemi124@gmail.com>
2021-08-31 17:29:29 -0400

    started to buiild some controllers

---
+++ eCommerceStarterCode/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET api/product
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            // get all products
+            var products = _context.Products;
+            return Ok(products);
+        }
+        // POST api/addnewproduct
+        [HttpPost("add")]
+        public IActionResult Post([FromBody] Product value)
+        {
+            _context.Products.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+    }
+}
