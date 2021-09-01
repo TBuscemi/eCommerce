35c6e31ca9f9404d807553c5842fdffe415dd126
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-09-01 15:11:31 -0500

    Updating main branch to be able to pull

--- eCommerceStarterCode/Models/SellerUpdateProduct.cs
+++ eCommerceStarterCode/Models/SellerUpdateProduct.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +10,16 @@ namespace eCommerceStarterCode.Models
     public class SellerUpdateProduct
     {
         public int NewProductID { get; set; }
-        public int SellerID { get; set; }
-        public int ProductID { get; set; }
+
+        [ForeignKey("Seller")]
+        [Required]
+        public int SellerID { get; }
+        public Seller Seller { get; set; }
+
+        [ForeignKey("Product")]
+        [Required]
+        public int ProductID { get; }
+        public Product Product { get; set; }
 
 
     }
