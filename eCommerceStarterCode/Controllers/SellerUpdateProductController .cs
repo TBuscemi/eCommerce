d5ca69eb359fb9a73205268e336376accc294fee
TBuscemi <thomasbuscemi124@gmail.com>
2021-09-01 16:21:23 -0400

    Added the SellerUpdateProductControler no functions added

---
+++ eCommerceStarterCode/Controllers/SellerUpdateProductController .cs
@@ -0,0 +1,21 @@
+﻿using eCommerceStarterCode.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Microsoft.AspNetCore.Components.Route("api/Cart")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public CartController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
