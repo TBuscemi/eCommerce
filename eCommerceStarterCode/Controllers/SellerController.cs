ed322d3e59e197357f43df124cf5c6fa77a9a6ff
TBuscemi <thomasbuscemi124@gmail.com>
2021-09-01 16:16:34 -0400

    Added the SellerControler no functions added

---
+++ eCommerceStarterCode/Controllers/SellerController.cs
@@ -0,0 +1,22 @@
+﻿using eCommerceStarterCode.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Microsoft.AspNetCore.Components.Route("api/Seller")]
+    [ApiController]
+    public class SellerController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public SellerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
