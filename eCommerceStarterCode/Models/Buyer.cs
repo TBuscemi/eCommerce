806885ee04c728cb89cbb8fa272ee7b14b46ba0a
TBuscemi <thomasbuscemi124@gmail.com>
2021-09-01 15:22:08 -0400

    Updated the buyer model with a FK

--- eCommerceStarterCode/Models/Buyer.cs
+++ eCommerceStarterCode/Models/Buyer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +10,12 @@ namespace eCommerceStarterCode.Models
     public class Buyer  
     {
         public int BuyerID { get; set; }
+
+        [ForeignKey("User")]
+        [Required]
         public int UserID { get; set; }
-
-
+        public User User { get; set; }
     }
+}
 
 }
--- eCommerceStarterCode/Models/Product.cs
+++ eCommerceStarterCode/Models/Product.cs
@@ -7,7 +7,7 @@ namespace eCommerceStarterCode.Models
 {
     public class Product
     {
-        public int ProductID { get; set; }
+        public int ProductID { get; set; } 
         public string ProductName { get; set; }
         public string ProductDescription { get; set; }
         public int ProductPrice { get; set; }
